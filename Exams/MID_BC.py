ab2c7c017a466bdba3a4be4ad559e64b88cb5feb
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-03-22 18:08:45 +0600

    Create MID_BC.py

---
+++ Exams/MID_BC.py
@@ -0,0 +1,67 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    def __init__(self, name:str, age:int):
+        self.name = name
+        self.age = age
+
+    @abstractmethod
+    def make_sound(self):
+         print("Make Sound")
+
+    @abstractmethod
+    def move(self):
+        print("The animal is flying")
+
+
+class Mammal(Animal):
+    def __init__(self, name:str, age:int, fru_colour:str):
+        super().__init__(name, age)
+        self.fur_colour = fru_colour
+
+    def give_birth(self):
+        print(f"{self.name} mammle is giving birth!")
+
+    def make_sound(self):
+        print(f"{self.name} mammle is making sound!")
+
+    def move(self):
+        print(f"{self.name} mammle is swimming.")
+
+
+class Bird(Animal):
+    def __init__(self, name:str, age:int, wing_span:float):
+        super().__inir__(name, age)
+        self.wing_spam = wing_span
+
+    def fly(self):
+        print(f"{self.name} bird is flying!")
+
+    def make_sounf(self):
+        print(f"{self.name} bird id making sound.")
+
+    def move(self):
+        print(f"{self.name} bird moves by flying.")
+
+
+class Reptile(Animal):
+    def __init__(self, name:str, age:int, scale_colour:str):
+        super().__init__(name, age)
+        self.scale_colour = scale_colour
+
+    def shed_skin(self):
+        print(f"{self.name} reptile is shedding it's skin!")
+
+    def make_sound(self):
+        print(f"{self.name} reptile is making sound now!")
+
+    def move(self):
+        print(f"{self.name} reptile is moving on it's feet.")
+
+
+class Lion(Mammal):
+    def __init__(self, name:str, age:int, fru_colour:str, mane_color:str):
+        super().__init__(name, age, fru_colour)
+        self.mane_color = mane_color
+
+    
