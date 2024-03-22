9def109fa683cdf6390ef9fe6a3381f0e5726647
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-03-22 20:46:35 +0600

    Update MID_BC.py

--- Exams/MID_BC.py
+++ Exams/MID_BC.py
@@ -14,6 +14,7 @@ class Animal(ABC):
         print("The animal is flying")
 
 
+
 class Mammal(Animal):
     def __init__(self, name:str, age:int, fru_colour:str):
         super().__init__(name, age)
@@ -59,9 +60,28 @@ class Reptile(Animal):
         print(f"{self.name} reptile is moving on it's feet.")
 
 
+
 class Lion(Mammal):
     def __init__(self, name:str, age:int, fru_colour:str, mane_color:str):
         super().__init__(name, age, fru_colour)
         self.mane_color = mane_color
+        print(f"The mane color of {self.name} lion is {self.mane_color}.")
 
-    
+
+class Eagle(Bird):
+    def __init__(self, name:str, age:int, wing_span:float, beak_length:float):
+        super().__init__(name, age, wing_span)
+        self.beak_length = beak_length
+        print(f"The beak length of {self.name} eagle is {self.beak_length} cm.")
+
+
+class Snake(Reptile):
+    def __init__(self, name:str, age:int, scale_colour:str, venomous:bool):
+        super().__init__(name, age, scale_colour)
+        self.venomous = venomous
+        if venomous:
+            print(f"{self.name} snake is venomous.")
+        else:
+            print(f"{self.name} snake is not venomous.")
+
+
